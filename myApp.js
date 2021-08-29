c3a4a018266ede72cd65cc4dd1c99d8485b1b35f
Rahardian Dewa Bimantara <tanardi94@users.noreply.github.com>
2021-08-29 07:25:10 +0000

    hrs e bener lah ya ini

--- myApp.js
+++ myApp.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 
 const mongoose = require('mongoose')
 
-var MONGOURI = 'mongodb+srv://nakit:silentcore@fcc.deaec.mongodb.net/FCC?retryWrites=true&w=majority'
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 let Person;
 
