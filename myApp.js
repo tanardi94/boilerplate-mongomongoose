436970c007fbbe778c9c9d935fd85d872e7ebdc5
Rahardian Dewa Bimantara <tanardi94@users.noreply.github.com>
2021-08-29 08:39:53 +0000

    hadehh

--- myApp.js
+++ myApp.js
@@ -92,9 +92,9 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-  Person.remove({name: nameToRemove}, (err, response) => {
-    if(err) return console.log(err);
-    done(null, response);
+  Person.remove({name: nameToRemove}, function(err,data) {
+    if(err) return done(err)
+    done(null, data)
   })
 };
 
