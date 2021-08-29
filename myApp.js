54e2f2f044a5400878a3a0428221e0f94396d0c3
Rahardian Dewa Bimantara <tanardi94@users.noreply.github.com>
2021-08-29 07:53:59 +0000

    tambah array of people

--- myApp.js
+++ myApp.js
@@ -26,8 +26,17 @@ const createAndSavePerson = (done) => {
 };
 
 
+var arrayOfPeople = [
+  {name: "Frankie", age: 74, favoriteFoods: ["Del Taco"]},
+  {name: "Sol", age: 76, favoriteFoods: ["roast chicken"]},
+  {name: "Robert", age: 78, favoriteFoods: ["wine"]}
+];
+
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, function (err, people) {
+    if (err) return console.log(err);
+    done(null, people);
+  });
 };
 
 const findPeopleByName = (personName, done) => {
