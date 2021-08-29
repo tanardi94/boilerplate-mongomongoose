7f20f3aa94caff5f6e78d01508cec7860ed0fbf0
Rahardian Dewa Bimantara <tanardi94@users.noreply.github.com>
2021-08-29 07:50:18 +0000

    coba create janeFonda

--- myApp.js
+++ myApp.js
@@ -17,7 +17,11 @@ let Person = mongoose.model("Person", personSchema)
 
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  var janeFonda = new Person({name: "Jane Fonda", age: 84, favoriteFoods: ["eggs", "fish", "fresh fruit"]});
+
+  janeFonda.save(function(err, data) {
+    if (err) return console.error(err);
+    done(null, data)
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
